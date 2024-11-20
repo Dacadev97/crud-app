b0f68088524dc65dd7a3570142258fb3e6d2a70a
Daca.dev <daca.dev97@gmail.com>
2024-11-19 19:24:41 -0500

    fix: update category binding in ModalComponent to use category name

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -16,7 +16,7 @@
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="category">Categoría</label>
           <select
-            v-model="formData.category"
+            v-model="formData.category.name"
             id="category"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           >
